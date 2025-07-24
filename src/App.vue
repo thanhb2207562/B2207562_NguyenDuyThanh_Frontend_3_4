e896cdaa5bfb768c54e4211ec8b019c0a218426b
thanhb2207562 <thanhb2207562@student.ctu.edu.vn>
2025-07-24 17:09:33 +0700

    Khoi tao dun an

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+// import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  // components: {
+  //   AppHeader,
+  // },
+};
+</script>
+
+<template>
+  <!-- <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div> -->
+  <h1> Hello . Vue.js</h1>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
