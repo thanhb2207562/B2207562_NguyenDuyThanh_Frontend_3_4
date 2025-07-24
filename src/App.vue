d5972c411dff3a0e48f6d63d348937e15b0631ac
thanhb2207562 <thanhb2207562@student.ctu.edu.vn>
2025-07-25 01:09:00 +0700

    Hien thi cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <script>
-// import AppHeader from "@/components/AppHeader.vue";
-
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-  // components: {
-  //   AppHeader,
-  // },
+  components: {
+    AppHeader,
+  },
 };
 </script>
-
 <template>
-  <!-- <div id="app">
+  <div id="app">
     <AppHeader />
     <div class="container mt-3">
       <router-view />
     </div>
-  </div> -->
-  <h1> Hello . Vue.js</h1>
+  </div>
 </template>
-
 <style>
 .page {
   max-width: 400px;
